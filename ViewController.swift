ffede0410208bf50e2c9085619fccfcc3362bfd6
Unknown <valera.nedbai.developer@gmail.com>
2020-07-03 11:50:05 +0300

    создал проект

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vkInterfaceApp
+//
+//  Created by MACUSER on 03.07.2020.
+//  Copyright © 2020 MACUSER. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
