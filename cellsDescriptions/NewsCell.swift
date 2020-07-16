55713bb9920368999eb491addf087de71aa2e2e2
makroguzov <valera.nedbai.developer@gmail.com>
2020-07-16 16:48:48 +0300

    готово

---
+++ cellsDescriptions/NewsCell.swift
@@ -0,0 +1,14 @@
+//
+//  NewsCell.swift
+//  vkInterfaceApp
+//
+//  Created by MACUSER on 14.07.2020.
+//  Copyright © 2020 MACUSER. All rights reserved.
+//
+
+import UIKit
+
+class NewsCell: UITableViewCell {
+    
+}
+
