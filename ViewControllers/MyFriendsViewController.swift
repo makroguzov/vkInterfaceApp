f1ad8c9b623842cfb972b570bd42fda12da7f7d6
makroguzov <valera.nedbai.developer@gmail.com>
2020-08-23 01:53:24 +0300

    занимась закладкой с друзьями

--- ViewControllers/MyFriendsViewController.swift
+++ ViewControllers/MyFriendsViewController.swift
@@ -8,7 +8,14 @@
 
 import UIKit
 
+extension MyFriendsViewController {
+    enum Sections: Int, CaseIterable {
+        case first, second, third
+    }
+}
+
 class MyFriendsViewController: UITableViewController {
+    private var mostImportantFriendCellModels = [FriendsCellModel]()
     private var friendCellModels = [FriendsCellModel]()
     private var friendsAmount: Int = 0
     
@@ -21,56 +28,122 @@ class MyFriendsViewController: UITableViewController {
 
 extension MyFriendsViewController {
     func getInitData() {
+        self.tableView.beginUpdates()
         getFriends()
     }
     
     func getFriends() {
-        NetworkService.shared.loadUserFriends(user_id: Session.shared.userId,
-                                              order: "hints", list_id: "", count: 5,
-                                              offset: 0, fields: "photo_200_orig,city,bdate",
-                                              name_case: "", ref: "") {
-            (userFriendsModel) in
-            self.tableView.beginUpdates()
+        NetworkService.shared.loadUserFriends(user_id: Session.shared.userId, order: "hints", list_id: "", count: 500, offset: 0, fields: "photo_200_orig,city,bdate", name_case: "", ref: "") { [weak self] (userFriendsModel) in
             
             let friends = userFriendsModel.response.users
             let friendsAmount = userFriendsModel.response.count
             
     
-            let numberOfRows = self.friendCellModels.count
-            self.tableView.insertRows(at: Array(numberOfRows ..< numberOfRows + friends.count).map { IndexPath(row: $0, section: 0) }, with: .automatic)
+            let numberOfRows = self?.friendCellModels.count ?? 0
+            self?.tableView.insertRows(at: Array(numberOfRows ..< numberOfRows + friends.count).map { IndexPath(row: $0, section: Sections.third.rawValue) }, with: .automatic)
             
             for friend in friends {
                 let model = FriendsCellModel(image: friend.photo_200_orig, name: friend.first_name, surename: friend.last_name, bdate: friend.bdate, city: friend.city)
-                self.friendCellModels.append(model)
+                self?.friendCellModels.append(model)
             }
             
-            self.friendsAmount = friendsAmount
+            
+            self?.friendsAmount = friendsAmount
             
             
-            self.tableView.endUpdates()
-            self.tableView.reloadData()
+            guard let friendCellModels = self?.friendCellModels else { return }
+            let mostImportantFriendCellModelsCount = friendCellModels.count < 5 ? friendCellModels.count : 5
+            self?.mostImportantFriendCellModels = Array(friendCellModels[0 ..< mostImportantFriendCellModelsCount])
+                
+            self?.tableView.insertRows(at: Array(0 ..< mostImportantFriendCellModelsCount).map { IndexPath(row: $0, section: Sections.second.rawValue) }, with: .automatic)
+            
+            
+            self?.tableView.endUpdates()
+            //self.tableView.reloadData()
         }
     }
 }
 
 extension MyFriendsViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return Sections.allCases.count
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return friendCellModels.count
+        let section = Sections(rawValue: section)
+        
+        switch section {
+        case .first:
+            return 1
+        case .second:
+            return mostImportantFriendCellModels.count
+        case .third:
+            return friendCellModels.count
+        default:
+            print("error: invalid section.")
+            fatalError()
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell") as? FriendCell else {
+        let section = Sections(rawValue: indexPath.section)
+        
+        switch section {
+        case .first:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "GroupFirstSectionCell") as? GroupFirstSectionCell else {
+                fatalError()
+            }
+            
+            return cell
+        case .second:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell") as? FriendCell else {
+                fatalError()
+            }
+            
+            cell.model = mostImportantFriendCellModels[indexPath.row]
+            
+            return cell
+        case .third:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "FriendCell") as? FriendCell else {
+                fatalError()
+            }
+            
+            cell.model = friendCellModels[indexPath.row]
+            
+            return cell
+        default:
+            print("error: invalid section.")
             fatalError()
         }
-        
-        cell.model = friendCellModels[indexPath.row]
-        
-        return cell
     }
     
 }
 extension MyFriendsViewController {
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let section = Sections(rawValue: indexPath.section)
+        
+        switch section {
+        case .first:
+            return 120
+        case .second, .third:
+            return 60
+        default:
+            return 0
+        }
+    }
     
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let section = Sections(rawValue: section)
+        
+        switch section {
+        case .first:
+            return 50
+        case .second, .third:
+            return 20
+        default:
+            return 0
+        }
+    }
 }
 
 
